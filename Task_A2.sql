d6bd502346f727b2db2eb42646c0a98cce65c565
Laura Dominguez Alvarez <128097662+adarual@users.noreply.github.com>
2023-11-22 10:57:37 +0000

    Update Task_A2.sql

--- Task_A2.sql
+++ Task_A2.sql
@@ -26,10 +26,54 @@ SELECT COUNT (DISTINCT trip_id) AS UniqueTrips,
 COUNT (*) AS NumberOfRows
 FROM Seattle_cycles_trip
 
---7) What is the average trip duration in minutes?
-SELECT AVG (tripduration) AS td
+--7) What is the average trip duration in minutes? **
+SELECT AVG (tripduration) AS avgtripduration
+FROM Seattle_cycles_trip
+WHERE tripduration = (tripduration * 60) 
+
+--8) From which station did the largest number of individual trips begin? **
+SELECT *
+FROM Seattle_cycles_trip
+GROUP BY from_station_name
+HAVING 
+
+--9) How many male members of the cycle hire scheme made trips in 2015? **
+SELECT trip_id 
+FROM Seattle_cycles_trip
+WHERE (YEAR (starttime) = 2015, AND (usertype) = Member, AND (gender) = Male)
+
+
+--10) How many rows do not have nulls for the column birthyear?
+SELECT *
+FROM Seattle_cycles_trip
+WHERE birthyear IS NOT NULL 
+
+--11) How old was the youngest rider that we know about?
+SELECT MAX (birthyear)
 FROM Seattle_cycles_trip
 
+--12) How many total trips started and ended at the same station?
+SELECT *
+FROM Seattle_cycles_trip 
+WHERE from_station_name = to_station_name
+
+--13) How many unique bikes were rented per year by short term pass holders? **
+SELECT *
+FROM Seattle_cycles_trip
+WHERE usertype LIKE ShortTermPass
+YEAR 
+GROUP BY usertype
+
+--14) What is the earliest and latest dates we have in the Seattle_weather_conditions table?
+SELECT 
+MIN (date) AS earliest, 
+MAX (date) AS latest
+FROM Seattle_weather_conditions
+
+--15) 
+
+SELECT *
+FROM Seattle_weather_conditions
 
 SELECT *
 FROM Seattle_cycles_trip
